d17e166abf040662fade72a6b1d3cff2144866e5
Serhii Spitsyn <finelineua@gmail.com>
2023-07-05 17:50:24 +0100

    Modify Practice Get Unique Char

--- c++/practice_get_unique_char.cpp
+++ c++/practice_get_unique_char.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 #include <bits/stdc++.h>
+#include <unordered_map>
+#include <vector>
 
 std::string extractWordsFromMultilineText(const std::string& multilineText);
 char getFirstUniqeSymbolToArray(const std::string& multilineText);
+char findFirstUniqueCharacter(const std::string& multilineText);
 
 int main() {
     std::string multilineText = "";
@@ -16,11 +19,15 @@ int main() {
         }
         multilineText += bufferLineText + '\n';
     }
-    std::string extractedWords = extractWordsFromMultilineText(multilineText);
-    std::cout << extractedWords << std::endl;
 
-    std::cout << extractedWords.length() << std::endl;
-    getFirstUniqeSymbolToArray(extractedWords);
+    std::string extractedWords = extractWordsFromMultilineText(multilineText);
+    char uniqueChar = findFirstUniqueCharacter(extractedWords);
+    
+    if (uniqueChar != '\0') {
+        std::cout << "First unique simbol: " << uniqueChar << std::endl;
+    } else {
+        std::cout << "There are no unique characters in a multiline" << std::endl;
+    }
 
     return 0;
 }
@@ -39,19 +46,42 @@ std::string extractWordsFromMultilineText(const std::string& multilineText) {
             }
         }
     }
-
-    if (!word.empty()) {
-        result += word + ' ';
-    }
-  
-    //set strint to lower case
-    transform(result.begin(), result.end(), result.begin(), ::tolower);
+    //Set strint to lower case if in necessary
+    //transform(result.begin(), result.end(), result.begin(), ::tolower);
 
     return result;
 }
 
-char getFirstUniqeSymbolToArray(const std::string& multilineText){
-    char ff;
-    return ff; 
-}
+char findFirstUniqueCharacter(const std::string& multilineText) {
+    std::vector<char> uniqueCharacters;
 
+    std::istringstream iss(multilineText);
+    std::string word;
+
+    while (iss >> word) {
+        std::unordered_map<char, int> charCount;
+
+        // Counting the number of occurrences of each character in a word
+        for (char c : word) {
+            charCount[c]++;
+        }
+
+        // Search for the first unique character in a word
+        for (char c : word) {
+            if (charCount[c] == 1) {
+                uniqueCharacters.push_back(c);
+                break;
+            }
+        }
+    }
+
+    // Search for the first unique character in an array
+    for (char c : uniqueCharacters) {
+        if (std::count(uniqueCharacters.begin(), uniqueCharacters.end(), c) == 1) {
+            return c;
+        }
+    }
+
+    // If the unique symbol is not found, we return '\0'
+    return '\0';
+}
